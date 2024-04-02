48fc052ef533e98e7f01ff586ff3c3b759d8993b
SasiKudasi <sasha.mittal@yandex.ru>
2024-04-02 17:48:36 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Jacks_House
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
